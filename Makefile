804eac757c391e75030a5671a432a77c9519bf2e
Mustafa Quraish <mustafaq9@gmail.com>
2022-08-15 08:01:47 -0400

    Add a better makefile with dependency tracking
    This makefile now only builds the .cpp files that have been modified
    since the last compilation as opposed to re-building everything.
    
    It also uses dependency-tracking to ensure that files are always
    re-built when any of their dependencies change.
    
    Run with `make -j` to compile the different units in parallel

---
+++ Makefile
@@ -0,0 +1,36 @@
+CC        = g++
+OBJ       = build
+SRCS     += $(wildcard src/*.cpp)
+DEPDIR   := $(OBJ)/deps
+DEPFILES := $(patsubst src/%.cpp,$(DEPDIR)/%.d,$(SRCS))
+OBJS      = $(patsubst src/%.cpp,$(OBJ)/%.o,$(SRCS))
+DEPFLAGS  = -MT $@ -MMD -MP -MF $(DEPDIR)/$*.d
+EXE       = nothing
+CFLAGS    = -g -w -std=c++20
+LDLIBS    = -lm -lstdc++
+
+
+all: $(EXE)
+
+$(DEPFILES):
+	@mkdir -p "$(@D)"
+
+$(EXE): $(OBJS) | $(BIN)
+	@echo "($(MODE)) Building final executable $@"
+	@$(CC) $(CFLAGS) $^ -o $@ $(LDLIBS)
+
+$(OBJ)/%.o : src/%.cpp $(DEPDIR)/%.d | $(DEPDIR)
+	@echo "($(MODE)) Compiling $@"
+	@mkdir -p "$(@D)"
+	@$(CC) $(DEPFLAGS) $(CFLAGS) -c $< -o $@ 
+
+$(OBJ):
+	@mkdir -p $@
+
+$(DEPDIR): 
+	@mkdir -p $@
+
+clean:
+	rm -rf build $(EXE)
+
+include $(wildcard $(DEPFILES))
--- src/parser.cpp
+++ src/parser.cpp
@@ -86,7 +86,7 @@ struct AST_NODE *parse_block(struct PARSER_STATUS *status, struct AST_NODE *pare
     };
 
     printf("ytttt    %s\n", status->current->literal_value.c_str());
-    consume_assert(status, T_RBRACE, "Expected '}' but received '%s'", status->current->literal_value);
+    consume_assert(status, T_RBRACE, "Expected '}' but received '%s'", status->current->literal_value.c_str());
     return node;
 }
 // @TODO : Add Floats
@@ -111,7 +111,7 @@ struct AST_NODE *parse_type(struct PARSER_STATUS *status, struct AST_NODE *paren
         while(status->current->type == T_LBRACKET) {
 
             consume(status);
-            consume_assert(status, T_RBRACKET, "Expected ']' but received '%s'", status->current->literal_value);
+            consume_assert(status, T_RBRACKET, "Expected ']' but received '%s'", status->current->literal_value.c_str());
 
             printf("Parsing\t::parse_factor\t::\tFound array type\n");
 
